093ca66b020cc8f94218e03f37e09f00ff414254
Will Jones <thefreethinker127@gmail.com>
2016-02-05 20:50:29 -0800

    Allocate array inside function

--- update_locations.c
+++ update_locations.c
@@ -3,19 +3,25 @@
 
 #define NELEMS(x)  (sizeof(x) / sizeof((x)[0]))
 
-void generate_random_array (float *array, size_t array_size, float bound) {
-  srand(time(NULL));
+float* generate_random_array (float *array, size_t array_size, int bound) {
+  array = (float *) malloc ( sizeof (float) * array_size); // Allocate array
+  
+  srand(time(NULL)); // Need a better way of doing this
+
   for (uint32_t i = 0; i < array_size; i++)
-    array[i] = rand() % 40;
+    array[i] = (rand() % (2 * bound)) - bound;
+
+  return array;
 }
 
  int main() { 
 
    uint32_t n = 10;
-   float x[n];
-   generate_random_array(x, n, 1);
+   float *x = generate_random_array(x, n, 5);
 
   for (uint32_t i = 0; i < n; i++)
     printf("%.6f\n", x[i]);
+
+  free(x); // Need to free x
 
 }
