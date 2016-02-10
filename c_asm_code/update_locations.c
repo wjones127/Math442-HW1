a0d18ed7137b3f599a0da9dbd79b6ae29af72241
aled1027 <a.led1027@gmail.com>
2016-02-10 06:27:31 -0800

    asm code working with uint64_t

--- c_asm_code/update_locations.c
+++ c_asm_code/update_locations.c
@@ -3,115 +3,100 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <assert.h>
 
-    /*
-     * jmp loop_cond
-     * loop_start:
-     * addl 1, counter
-     * addl counter, sum
-     * loop_cond:
-     * cmpl 17, counter
-     * jl loop_start
-     * end_loop:
-     */
-
-    /* Which of the following is better and why?
-    asm ("movq %1, %0;\n"
-         "addq %2, %0\n"
-         */
-    //asm ("movq %1, %%rax;\n"
-    //     "movq %2, %%rbx\n"
-    //     "addq %%rax, %%rbx\n"
-    //     "movq %%rbx, %0;"
-    //     :"=r"(c)        /* output c = %0  */
-    //     :"r"(a), "r"(b) /* input a = %1, b = %2*/
-    //     :"%rax"         /* clobbered register */
-    //    );
+void triangle_number()
+{
 
     // rbx = counter
     // rax = sum
-    //asm (
-    //     // pre
-    //     "movq $0, %%rbx;\n"
-    //     "movq $0, %%rax\n"
-    //     "movq %1, %%rcx\n"
-
-    //     // loop
-    //     "loop_start:\n"
-    //     "addq %%rbx, %%rax\n"
-    //     "addq $1, %%rbx\n"
-
-    //     // loop condition
-    //     "loop_cond:\n"
-    //     "cmpq %%rcx, %%rbx\n"
-    //     "jl loop_start\n"
-
-    //     // loop end
-    //     "movq %%rax, %0\n"
-    //     :"=r"(c)        /* output c = %0  */
-    //     :"r"(a) /* input a = %1, b = %2*/
-    //     :"%rax"         /* clobbered register */
-    //     );    
-    /*printf("b = %llu\n", b);*/
-
-    //uint64_t px[2] = {11, 22};
-    //uint64_t y = 0;
-    //printf("px[0] = %" PRId64 "\n", px[0]);
-    //printf("px[1] = %" PRId64 "\n", px[1]);
-    //printf("y = %" PRId64 "\n", y);
-    //asm (
-    //     "movq %1, %%rbx;\n"
-    //     "movq 8(%%rbx), %%rax\n"
-    //     "movq %%rax, %0\n"
-    //     :"=r"(y)        /* output c = %0  */
-    //     :"r"(px) /* input a = %1, b = %2*/
-    //     :"%rax"         /* clobbered register */
-    //     );    
-    //printf("px[0] = %" PRId64 "\n", px[0]);
-    //printf("px[1] = %" PRId64 "\n", px[1]);
-    //printf("y = %" PRId64 "\n", y);
-
-void run() 
-{
-    uint64_t size = 4;
-    uint64_t pos[4] = {11, 22, 33, 2};
-    uint64_t vel[4] = {1, 0, 0, 0};
-    uint64_t out[4] = {0, 0, 0, 0};
+    uint64_t a = 10;
+    uint64_t b = 0;
     asm (
-        // pre
-         "movq %1, %%rsi\n" /* position array */
-         "movq %2, %%rbx;\n" /* velocity array */
+         // rax = sum
+         // pre
+         "movq %1, %%rbx;\n"
+         "movq $0, %%rax\n"
+         "movq $0, %%rcx\n"
 
-         "movq $0, %%rcx\n" /* counter */
-         "movq %3, %%rdx\n"  /* size */
-         "movq %0, %%rax\n" /* position array */
+         // loop
+         "loop_start:\n"
+         "addq %%rcx, %%rax\n"
+         "addq $1, %%rcx\n"
 
-         //"loop_start:\n"
-         //
-         "movq (%%rsi, %%rcx, 8), %%r8\n"
-         "addq %%r8, (%%rbx, %%rcx, 8)\n"
-         "movq (%%rbx, %%rcx, 8), %%r8\n"
-         //"addq %%r8, (%%rax, %%rcx, 8)\n" /* problem here --- how do I access elements of %0? */
+         "cmpq %%rbx, %%rcx\n"
+         "jl loop_start\n"
 
-         //"addq $1, %%rcx\n" /* todo change to incq */
-
-         // loop condition
-         //"cmpq %%rdx, %%rcx\n"
-         //"jl loop_start\n"
-
-         // end_condition
+         // loop end
          "movq %%rax, %0\n"
-
-         :"=g"(out)         /* output c = %0  */
-         :"r"(pos), "r"(vel), "r"(size) /* %1 %2 %3*/
+         :"=r"(b)        /* output c = %0  */
+         :"r"(a)
          :"%rax"         /* clobbered register */
          );    
-    printf("out[0] = %" PRId64 "\n", out[0]);
-    printf("out[1] = %" PRId64 "\n", out[1]);
-    printf("out[2] = %" PRId64 "\n", out[2]);
+    printf("a = %" PRId64 "\n", a);
+    printf("b = %" PRId64 "\n", b);
+}
 
+void combine_arrays(uint64_t *arr, uint64_t *add, uint64_t size)
+{
+    /* Adds the number add to all elements in array */
+
+    uint64_t out;
+    asm (
+         //"movq %1, %%rbx;\n"
+         "movq %1, %%rax\n" // target array
+         "movq %2, %%rbx\n" // add array
+         "movq %3, %%rdx\n" // size 
+         "movq $0, %%rcx\n" // counter
+
+         "loop_start3:\n"
+         "movq (%%rbx, %%rcx, 8), %%rsi\n"
+         "addq %%rsi, (%%rax, %%rcx, 8)\n"
+         "incq %%rcx\n"
+
+         "cmpq %%rdx, %%rcx\n"
+         "jl loop_start3\n"
+
+         // loop end
+         "movq $0, %0\n"
+         :"=r"(out)
+         :"r"(arr), "r"(add), "r"(size)
+         :"%rax"         /* clobbered register */
+         );    
+    assert(out == 0);
+}
+
+void double_arr() 
+{
+    printf("here\n");
+    //double arr[2] = {1.0, 2.0}
+    //uint64_t length = 2;
+    //uint64_t out
+    //asm (
+    //    "xorpd   xmm0, xmm0\n"
+    //    "addsd   xmm0, [rdi]\n"
+    //    "add     rdi, 8\n"
+    //    "dec     rsi\n"
+    //    "jnz     next\n"
+
+
+    //     // loop end
+    //     "movq $0, %0\n"
+    //     :"=r"(out)
+    //     :"r"(arr), "r"(add), "r"(size)
+    //     :"%rax"         /* clobbered register */
+    //     );    
 
 }
+
+//void update_coords_better(float *x, float *y, float *z, float *vx, float *vy,
+//                   float *vz, size_t size) 
+//{
+//    combine_arrays_better(x, vx, size);
+//    combine_arrays_better(y, vy, size);
+//    combine_arrays_better(z, vy, size);
+//
+//}
 
 void update_coords(float *x, float *y, float *z, float *vx, float *vy,
                    float *vz, size_t size) 
@@ -127,6 +112,24 @@ int main(int argc, char *argv[])
 {
     assert(argc > 0);
     assert(argv);
-    run();
+
+    uint64_t size = 4;
+    uint64_t pos[4] = {10, 20, 30, 40};
+    uint64_t vel[4] = {1, 2, 3, 4};
+
+    //printf("%lf\n", pos[0]);
+    //printf("%lf\n", pos[1]);
+    //printf("%lf\n", pos[2]);
+
+    combine_arrays(pos, vel, size);
+
+    //printf("%lf\n", pos[0]);
+    //printf("%lf\n", pos[1]);
+    //printf("%lf\n", pos[2]);
+
+    printf("[0] = %" PRId64 "\n", pos[0]);
+    printf("[1] = %" PRId64 "\n", pos[1]);
+    printf("[2] = %" PRId64 "\n", pos[2]);
+    printf("[3] = %" PRId64 "\n", pos[3]);
     return 0;
 }
